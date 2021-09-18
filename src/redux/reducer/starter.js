b1baf2ca73e3d3ccd5cb4d099790dff5c0e1a028
dackmagic115 <pakaphon23@gmail.com>
2021-09-18 16:22:36 +0700

    change name

--- src/redux/reducer/starter.js
+++ src/redux/reducer/starter.js
@@ -1,12 +1,12 @@
 import { PRE_LOAD_DATA, LOAD_DATA } from "../actions/type";
 
-export default function starter(state = { loading: false, news: {} }, action) {
+export default function starter(state = { loading: false, data: {} }, action) {
   switch (action.type) {
     case PRE_LOAD_DATA:
       return { ...state, loading: true };
     case LOAD_DATA:
       console.log(action);
-      return { ...state, news: action.data, loading: false };
+      return { ...state, data: action.data, loading: false };
     default:
       return state;
   }
